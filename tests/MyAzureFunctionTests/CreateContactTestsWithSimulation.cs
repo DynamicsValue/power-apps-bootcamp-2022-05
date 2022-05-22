c9587460075c47a55dc063d21429baf76bd0ea3c
Jordi <jordi.montana@gmail.com>
2022-05-22 03:58:34 +0200

    Added uncommited changes after demo

--- tests/MyAzureFunctionTests/CreateContactTestsWithSimulation.cs
+++ tests/MyAzureFunctionTests/CreateContactTestsWithSimulation.cs
@@ -12,7 +12,25 @@ namespace MyAzureFunctionTests
     public class CreateContactTestsWithSimulation : FakeXrmEasyPipelineTestsBase
     {
         [Fact]
-        public async void Should_create_contact_with_pipeline()
+        public async void Should_create_contact_and_task_with_pipeline()
+        {
+            _context.RegisterPluginStep<FollowUpPlugin>("Create", ProcessingStepStage.Postoperation, ProcessingStepMode.Synchronous, rank: 1, primaryEntityTypeCode: Contact.EntityTypeCode);
+
+            var result = await CreateContactFn.CreateContact(_service, "Joe", "joe@satriani.com");
+            Assert.True(result.Succeeded);
+
+            var contacts = _context.CreateQuery("contact").ToList();
+            Assert.Single(contacts);
+
+            var tasks = _context.CreateQuery<Task>().ToList();
+            Assert.Single(tasks);
+
+            Assert.Equal("Joe", contacts[0]["firstname"]);
+            Assert.Equal("joe@satriani.com", contacts[0]["emailaddress1"]);
+        }
+
+        [Fact]
+        public async void Should_create_contact_with_pipeline_and_audit()
         {
             _context.RegisterPluginStep<FollowUpPlugin>("Create", ProcessingStepStage.Postoperation, ProcessingStepMode.Synchronous, rank: 1, primaryEntityTypeCode: Contact.EntityTypeCode);
 
@@ -34,6 +52,9 @@ namespace MyAzureFunctionTests
 
             Assert.Equal("Create", auditedStep.MessageName);
             Assert.Equal(typeof(FollowUpPlugin), auditedStep.PluginAssemblyType);
+
+
+       
         }
 
         
